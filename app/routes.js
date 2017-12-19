efeb2b8d0e1adbca9d37b8562b532c0fc5660c76
Kanishk Gandhi <kanishk050296@gmail.com>
2017-12-20 01:10:35 +0530

    page1

--- app/routes.js
+++ app/routes.js
@@ -1,42 +1,6 @@
 //var Todo = require('./models/todo');
 var stateInfo = {}
 module.exports = function (app) {
-
-    // api ---------------------------------------------------------------------
-    // get all todos
-	//app.get('/api/todos', function (req, res) {
-    	//    // use mongoose to get all todos in the database
-    	//    getTodos(res);
-    	//});
-
-    	//// create todo and send back all todos after creation
-    	//app.post('/api/todos', function (req, res) {
-
-    	//    // create a todo, information comes from AJAX request from Angular
-    	//    Todo.create({
-    	//        text: req.body.text,
-    	//        done: false
-    	//    }, function (err, todo) {
-    	//        if (err)
-    	//            res.send(err);
-
-    	//        // get and return all the todos after you create another
-    	//        getTodos(res);
-    	//    });
-
-    	//});
-
-    	//// delete a todo
-    	//app.delete('/api/todos/:todo_id', function (req, res) {
-    	//    Todo.remove({
-    	//        _id: req.params.todo_id
-    	//    }, function (err, todo) {
-    	//        if (err)
-    	//            res.send(err);
-
-    	//        getTodos(res);
-    	//    });
-    	//});
 	app.get('/state',function (req,res){
 		console.log("get state");
 		var senderID = req.psid;
@@ -56,20 +20,14 @@ module.exports = function (app) {
 			res.json(stateInfo[threadID]);
 		}
 	});
-	app.post('/state',function (req,res){
+	app.post('/preference',function (req,res){
 		var senderID = req.psid;
 		var threadID = req.tid;
-		if(stateInfo[threadID]["state"] === 1){
-			var loc = req.loc;
-			stateInfo[threadID]["users"].indexOf(senderID) === -1 ? stateInfo[threadID]["users"].push(senderID):pass;
-			stateInfo[threadID]["locations"][senderID]= loc;
-		}
-		else if(stateInfo[threadID]["state" === 0]){
-			var preference = req.pref;
-			stateInfo[threadID]["state"] = 1;
-			stateInfo[threadID]["users"].indexOf(senderID) === -1 ? stateInfo[threadID]["users"].push(senderID):pass;
-			stateInfo[threadID]["preference"]= pref;
-		}
+		var loc = req.loc;
+		var preference = req.pref;
+		stateInfo[threadID]["state"] = 1;
+		stateInfo[threadID]["users"].indexOf(senderID) === -1 ? stateInfo[threadID]["users"].push(senderID):pass;
+		stateInfo[threadID]["preference"]= pref;
 		res.json(stateInfo[threadID])
 	});
     // application -------------------------------------------------------------
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
 git add .
-git commit -m "deploy"
+git commit -m  "$msg"
 git push
 git push heroku
+heroku logs --tail
