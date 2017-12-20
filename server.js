4bf31ae9292cbcebd85ce65fcb867d9455baadc3
Kanishk Gandhi <kanishk050296@gmail.com>
2017-12-20 21:29:23 +0530

    addgetlocation

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
+app.engine('html', require('ejs').renderFile);
 var state_info = {};
 
 // routes ======================================================================
